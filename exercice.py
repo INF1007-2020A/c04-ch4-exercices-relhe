9a5be9fda5049b2ee45f252f3b793e7c1081a42b
relhe <renel.lherisson@polymtl.ca>
2020-09-16 07:52:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -8,21 +8,26 @@ def is_even_len(string: str) -> bool:
     else :
         return False
 
-
 def remove_third_char(string: str) -> str:
     begin =string[0:2]
     end=string[3:]
     return begin+end
 
-
-
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    
-    pass
+    for i in range(len(string)) : 
+        string=string[:i]+ new_char + string [i+1:]
+        return string
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    pass
+    for i in range(len(string)):
+        nombre=0
+        if string[i] == char :
+            nombre +=1
+           
+    return nombre
+
+            
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
@@ -42,7 +47,7 @@ def main() -> None:
     chaine = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_number_of_char(chaine, 'l')}")
+    print(f"Le nombre d'occurrence de l dans hello world est : {get_number_of_char(chaine, 'l')}")
     
     chaine = "Baby shark doo doo doo doo doo doo"
     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
